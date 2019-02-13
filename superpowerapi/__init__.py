ff7db5606d58bdd3df8edfca478ee75956263e92
Ali İhsan Karabal <aliihsankarabal@eskisehir.edu.tr>
2019-02-13 19:05:21 +0300

    Create __init__.py

---
+++ superpowerapi/__init__.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, request
+from flask_cors import CORS
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+app.debug = False
+api = Api(app)
+CORS(app)
+
+ODBC_URL = os.environ.get('ODBC_URL')
+
+import superpowerapi.api
